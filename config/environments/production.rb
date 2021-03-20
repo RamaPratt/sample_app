9346707e317a4923a5f72156dedb3eda8e7a7d15
RamaPratt <ramapratt@gmail.com>
2021-03-20 13:17:47 -0700

    adfadfadf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://git.heroku.com/calm-mesa-34202.git'
+  host = 'sample_app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
