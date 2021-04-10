f75b68bfd2a15725861865526fe0bf34d20b9ebe
RamaPratt <ramapratt@gmail.com>
2021-04-10 15:51:25 -0700

    Add user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,14 +5,15 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     @micropost.image.attach(params[:micropost][:image])
-    if @micropost.saveflash[:success] = "Micropost created!"
+    if @micropost.save
+      flash[:success] = "Micropost created!"
       redirect_to root_url
     else
       @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
-  
+
   def destroy
     @micropost.destroy
     flash[:success] = "Micropost deleted"
